688b5c84f66cc2fec40f2fa38989edbc93e667b3
ajb0411 <46768729+ajb0411@users.noreply.github.com>
2019-01-17 14:19:12 -0600

    Updated constants

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,18 +1,20 @@
 module Constants
-  TIMEZONE = 'Eastern Time (US & Canada)'
+  TIMEZONE = 'Central Time (US & Canada)'
   DAY_END = 1
-  BUILD_MEETINGS = 2
-  PRE_MEETINGS = 1
-  PRE_HOURS = 25
-  DEFAULT_LOGIN = "signup@team900.org"
-  TEAM_NAME = 'Zebracorns'
-  TEAM_NUMBER = '900'
-  SCHOOL_NAME_REPLACEMENT = 'School'
-  GENDERS = ['Male', 'Female', 'Other']
-  FRESHMAN_HOURS = 6
-  SOPHOMORE_HOURS = 8
-  JUNIOR_HOURS = 10
-  SENIOR_HOURS = 10
-  LEADERSHIP_HOURS = 15
-  FLEX_HOURS = 15
+  BUILD_MEETINGS = 0
+  PRE_MEETINGS = 0
+  PRE_HOURS = 0
+  DEFAULT_LOGIN = "signup@team5212.com"
+  TEAM_NAME = 'TAMSformers Robotics'
+  TEAM_NUMBER = '5212'
+  SCHOOL_NAME_REPLACEMENT = 'TAMS'
+  GENDERS = ['Male', 'Female', 'Prefer not to Specify']
+  FRESHMAN_HOURS = 1
+  SOPHOMORE_HOURS = 1
+  JUNIOR_HOURS = 1
+  SENIOR_HOURS = 1
+  LEADERSHIP_HOURS = 0
+  FLEX_HOURS = 0
+  MENTOR_NAME = 'Raul A.'
+  MENTOR_EMAIL = 'tamsformers@gmail.com'
 end
